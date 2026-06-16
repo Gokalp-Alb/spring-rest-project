cf324c65141a01af446cadd036f0b2184b3c1fb9
Gokalp-Alb <gokalp.albayrak@ug.bilkent.edu.tr>
2026-06-16 12:44:32 +0300

    hidden admin's credentials in local properties

--- src/main/java/com/springrest/springrestproject/config/AdminInitializerConfig.java
+++ src/main/java/com/springrest/springrestproject/config/AdminInitializerConfig.java
@@ -4,6 +4,7 @@ import com.springrest.springrestproject.model.AppUser;
 import com.springrest.springrestproject.model.Role;
 import com.springrest.springrestproject.repository.IUserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,20 +17,22 @@ public class AdminInitializerConfig {
     private final IUserRepo userRepo;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${app.admin.username}")
+    private String adminUsername;
+
+    @Value("${app.admin.password}")
+    private String adminPassword;
+
     @Bean
     public CommandLineRunner initializeDefaultAdmin() {
         return args -> {
             boolean adminExists = userRepo.findAll().stream()
                     .anyMatch(user -> user.getRole() == Role.ADMIN);
-
             if (!adminExists) {
-
                 AppUser defaultAdmin = new AppUser();
-                defaultAdmin.setUsername("admin");
-
-                defaultAdmin.setPassword(passwordEncoder.encode("admin123"));
+                defaultAdmin.setUsername(adminUsername);
+                defaultAdmin.setPassword(passwordEncoder.encode(adminPassword));
                 defaultAdmin.setRole(Role.ADMIN);
-
                 userRepo.save(defaultAdmin);
             }
         };
